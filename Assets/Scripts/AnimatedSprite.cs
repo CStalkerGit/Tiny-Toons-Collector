4f823453422d2b6c7d040e4b545465c9d8b5dd37
CStalker <cstalker.coder@gmail.com>
2023-03-11 02:48:50 +0300

    new tiles

---
+++ Assets/Scripts/AnimatedSprite.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedSprite : MonoBehaviour
+{
+    public Sprite[] sprites;
+    public float animSpeed = 0.25f;
+    float time;
+    int frame;
+
+    SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+        frame = 0;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (sprites.Length == 0) return;
+
+        time += Time.deltaTime;
+        if (time >= animSpeed)
+        {
+            time = 0;
+            frame++;
+            if (frame >= sprites.Length) frame = 0;
+            spriteRenderer.sprite = sprites[frame];
+        }
+    }
+}
---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform follow;
+    public int x1;
+    public int x2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!follow) return;
+
+        Vector2 pos = follow.position;
+
+        pos.x = Mathf.Clamp(pos.x, x1 + 5, x2 - 4);
+
+        transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+    }
+}
