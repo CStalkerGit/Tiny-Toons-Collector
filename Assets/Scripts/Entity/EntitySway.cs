9dba0604a9bdfe5d314b6d3e24a5e0e109b85188
CStalker <cstalker.coder@gmail.com>
2023-05-05 23:18:47 +0300

    checkpoint system

---
+++ Assets/Scripts/Entity/EntitySway.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Entity))]
+public class EntitySway : MonoBehaviour
+{
+    Entity entity;
+    Vector3 origin;
+    float sin;
+
+    void Awake()
+    {
+        entity = GetComponent<Entity>();
+        origin = transform.position;
+        sin = Random.Range(-Mathf.PI, Mathf.PI);
+    }
+
+    void FixedUpdate()
+    {
+        var pos = origin;
+        pos.y += Mathf.Sin(sin) * 0.2f;
+        sin += Time.deltaTime * 3;
+        if (sin > Mathf.PI) sin -= Mathf.PI * 2;
+        transform.position = pos;
+        entity.pos = pos;
+    }
+}
---
+++ Assets/Scripts/Trigger/Checkpoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Entity))]
+public class Checkpoint : MonoBehaviour
+{
+    public Sprite activated;
+    public Sprite waiting;
+
+    Entity entity;
+
+    void Awake()
+    {
+        entity = GetComponent<Entity>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Player.IsCollision(entity))
+        {
+
+        }
+    }
+}
